f0cc5dfeca0d9f03aac24e3f33f5104cdc2b1f78
Vyacheslavorlov777 <Vyacheslavorlov777@gmail.com>
2024-04-17 16:42:39 +0300

    dont see

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-import {
-    mkfile, mkdir, isDirectory, isFile, map, getChildren,
-  } from '@hexlet/immutable-fs-trees';
+// import {
+//     mkfile, mkdir, isDirectory, isFile, map, getChildren,
+//   } from '@hexlet/immutable-fs-trees';
 
-  const createChilds = mkdir('nodejs-package', [mkfile('Makefile'), mkfile('README.md'), mkdir('dist'), mkdir('__tests__', [mkfile('half.test.js', { type: 'text/javascript' })]), mkfile('babel.config.js', { type: 'text/javascript' }), mkdir('node_modules', [mkdir('@babel', [mkdir('cli', [mkfile('LICENSE')])])], { owner: 'root', hidden: false } )], { hidden: true });
-  console.log(createChilds);
+//   const createChilds = mkdir('nodejs-package', [mkfile('Makefile'), mkfile('README.md'), mkdir('dist'), mkdir('__tests__', [mkfile('half.test.js', { type: 'text/javascript' })]), mkfile('babel.config.js', { type: 'text/javascript' }), mkdir('node_modules', [mkdir('@babel', [mkdir('cli', [mkfile('LICENSE')])])], { owner: 'root', hidden: false } )], { hidden: true });
+//   console.log(createChilds);
+
+
+window.addEventListener('load', function() {
+    document.querySelector('.loader').style.display = 'none';
+    document.querySelector('img').classList.remove('hidden');
+});
